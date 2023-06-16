3613b8a3a9ce8810cac2d2a20eac7353072fd530
cloudmoonocus <f356660222@foxmail.com>
2023-06-16 08:55:14 +0800

    ⚒️ 修改路由模式为哈希模式

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import message from '../utils/message'
 import { useUserStore } from '../stores/user'
 
@@ -153,7 +153,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     scrollBehavior(to, from, savedPosition) {
         if (savedPosition) {
             return savedPosition
